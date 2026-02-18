f770620bfd6f233a040e7bee8b605a55fe308b51
Olha <189846437+sibaraku@users.noreply.github.com>
2026-02-18 09:19:42 +0200

    Install basic packages

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Sequelize application!' });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+})
