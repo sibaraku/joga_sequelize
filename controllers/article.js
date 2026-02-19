a3b860f8c4dfed06e5f480c1074cc6c0dd9eb39f
Olha <189846437+sibaraku@users.noreply.github.com>
2026-02-19 09:32:20 +0200

    Getted article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
-
 const Article = require('../models/article')(sequelize, Sequelize.DataTypes);
 
 const getAllArticles = (req, res) => {
@@ -9,9 +8,22 @@ const getAllArticles = (req, res) => {
       res.status(200).json({ articles });
     })
     .catch((error) => {
+      return res.status(500).send(error.message);
+    });
+};
+
+const getArticleBySlug = (req, res) => {
+  Article.findOne({
+    where: { slug: req.params.slug },
+  })
+    .then((article) => {
+      console.log(article);
+      res.status(200).json({ article });
+    })
+    .catch((error) => {
       res.status(500).send(error.message);
     });
 };
 
 
-module.exports = { getAllArticles };
+module.exports = { getAllArticles, getArticleBySlug };
--- index.js
+++ index.js
@@ -19,6 +19,7 @@ sequelize
 
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
+app.use('/article', articleRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
